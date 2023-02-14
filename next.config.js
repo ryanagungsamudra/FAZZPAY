7ed853a6d2b19449709a1b31be3634d17fb6131b
ryanagungsamudra <ryansamudra67@gmail.com>
2023-02-15 03:13:38 +0700

    redeploy again

--- next.config.js
+++ next.config.js
@@ -6,12 +6,12 @@ const nextConfig = {
   images: {
     // formats: ['image/avif', 'image/webp'],
     remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '5000',
-        pathname: '/uploads/images/**',
-      },
+      // {
+      //   protocol: 'http',
+      //   hostname: 'localhost',
+      //   port: '5000',
+      //   pathname: '/uploads/images/**',
+      // },
       {
         protocol: 'https',
         hostname: 'yannnfazzpay.vercel.app',
